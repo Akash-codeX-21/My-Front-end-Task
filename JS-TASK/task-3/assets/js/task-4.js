37d4470a025dbd4f8c5e47d6132ad4846027df48
Akash-codeX-21 <heartless1577@gmail.com>
2026-08-19 18:24:47 +0530

    java script day 3 task update

---
+++ JS-TASK/task-3/assets/js/task-4.js
@@ -0,0 +1,10 @@
+let agee = 22;
+let hasId = true;
+
+console.log(agee >= 18 && hasId);
+
+
+console.log(agee < 18 || hasId);
+
+
+console.log(!hasId);
---
+++ JS-TASK/task-3/assets/js/task-5.js
@@ -0,0 +1,22 @@
+let count = 10;
+
+count++;
+console.log(count);
+
+count++;
+console.log(count);
+
+count--;
+console.log(count);
+
+count--;
+console.log(count);
+
+
+
+
+if (count % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
